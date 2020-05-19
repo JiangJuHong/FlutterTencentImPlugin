08384b246979d55fd254b4ca218c9707c82d4236
jiangjuhong <jjh@huic.top>
2020-05-19 11:26:17 +0800

    修复 message toJson 报错

--- lib/entity/message_entity.dart
+++ lib/entity/message_entity.dart
@@ -101,9 +101,7 @@ class MessageEntity {
     peerReaded = json['peerReaded'];
     sender = json['sender'];
     sessionId = json['sessionId'];
-    userInfo = json['userInfo'] == null
-        ? null
-        : UserInfoEntity.fromJson(json['userInfo']);
+    userInfo = json['userInfo'] == null ? null : UserInfoEntity.fromJson(json['userInfo']);
     if (json['status'] != null) {
       for (var item in MessageStatusEnum.values) {
         if (EnumUtil.getEnumName(item) == json['status']) {
@@ -126,9 +124,10 @@ class MessageEntity {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['customInt'] = this.customInt;
     data['read'] = this.read;
-    if (this.elemList != null) {
-      data['elemList'] = elemList == null ? null : jsonEncode(elemList);
-    }
+    // elemList 节点不需要反序列化到后台
+//    if (this.elemList != null) {
+//      data['elemList'] = elemList == null ? null : jsonEncode(elemList);
+//    }
     data['self'] = this.self;
     data['id'] = this.id;
     data['uniqueId'] = this.uniqueId;
@@ -140,8 +139,7 @@ class MessageEntity {
     data['sessionType'] = EnumUtil.getEnumName(this.sessionType);
     data['userInfo'] = this.userInfo == null ? null : this.userInfo.toJson();
 
-    data['status'] =
-        this.status == null ? null : EnumUtil.getEnumName(this.status);
+    data['status'] = this.status == null ? null : EnumUtil.getEnumName(this.status);
     data['rand'] = this.rand;
     data['seq'] = this.seq;
     data['note'] = this.note;
@@ -149,16 +147,8 @@ class MessageEntity {
   }
 
   @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is MessageEntity &&
-          runtimeType == other.runtimeType &&
-          rand == other.rand &&
-          seq == other.seq &&
-          self == other.self &&
-          sessionId == other.sessionId;
+  bool operator ==(Object other) => identical(this, other) || other is MessageEntity && runtimeType == other.runtimeType && rand == other.rand && seq == other.seq && self == other.self && sessionId == other.sessionId;
 
   @override
-  int get hashCode =>
-      rand.hashCode ^ seq.hashCode ^ self.hashCode ^ sessionId.hashCode;
+  int get hashCode => rand.hashCode ^ seq.hashCode ^ self.hashCode ^ sessionId.hashCode;
 }
