4584ffb7554191cc61103bb2a333b65ce3626729
jiangjuhong <jjh@huic.top>
2020-08-10 21:04:45 +0800

    修复IOS运行报错

--- ios/Classes/message/TextMessageNode.swift
+++ ios/Classes/message/TextMessageNode.swift
@@ -6,20 +6,20 @@ import ImSDK
 //
 //  Created by 蒋具宏 on 2020/3/13.
 //  文本消息节点
-public class TextMessageNode : AbstractMessageNode{
-    
-    override func getSendMessage(params: [String : Any]) -> TIMMessage? {
+public class TextMessageNode: AbstractMessageNode {
+
+    override func getSendMessage(params: [String: Any]) -> TIMMessage? {
         let message = TIMMessage();
         let textElem = TIMTextElem();
         textElem.text = getParam(params: params, paramKey: "content")!;
         message.add(textElem);
         return message;
     }
-    
+
     override func getNote(elem: TIMElem) -> String {
-        return (elem as! TIMTextElem).text!;
+        return (elem as! TIMTextElem).text ?? "";
     }
-    
+
     override func analysis(elem: TIMElem) -> AbstractMessageEntity {
         let entity = TextMessageEntity();
         entity.content = (elem as! TIMTextElem).text;
