e5fb7c3e2cf3da21632eee08f1ffb5c836e7bb0e
jiangjuhong <jjh@huic.top>
2020-12-19 17:29:36 +0800

    增加生成签名的算法

--- example/lib/page/login.dart
+++ example/lib/page/login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:tencent_im_plugin/tencent_im_plugin.dart';
+import 'package:tencent_im_plugin_example/utils/GenerateTestUserSig.dart';
 
 /// 登录页面
 class Login extends StatefulWidget {
@@ -9,19 +10,30 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  TextEditingController _controller = TextEditingController();
+
   /// 用户名
-  String _userName = "";
+  String _userName = "dev";
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.text = _userName;
+  }
 
   /// 登录按钮点击事件
   _onLogin() async {
+    String sign = GenerateTestUserSig(
+      sdkappid: 1400294314,
+      key: "706da51f9280812611bcc80b5182b1c5554db8d053bc00b8a37ae8cba887f6a7",
+    ).genSig(identifier: _userName, expire: 24 * 60 * 1000);
+
     await TencentImPlugin.login(
-      userID: "dev",
-      userSig:
-          "eJwtzEELgjAYxvHvsnPIu7U5EzoEQRDWITWqW7KVr8MaS8Yi*u6Zenx*D-w-pMjyyGtHUsIiILNho9KPDm84sNJ*4pcyV2tRkZRyALbgc8rHRweLTvcuhGAAMGqH7d9iEAKSWMipgve*ulZS1vKoT83uXO6rkAWzaoqtgVAdNpecu9Y-38aXtE6W5PsDr9ExPw__",
+      userID: _userName,
+      userSig: sign,
     );
 
-    Navigator.pushNamedAndRemoveUntil(
-        context, "/main", (Route<dynamic> route) => false);
+    Navigator.pushNamedAndRemoveUntil(context, "/main", (Route<dynamic> route) => false);
   }
 
   @override
@@ -40,6 +52,7 @@ class _LoginState extends State<Login> {
                 decoration: InputDecoration(hintText: "请输入用户名"),
                 textAlign: TextAlign.center,
                 onChanged: (value) => this.setState(() => _userName = value),
+                controller: _controller,
               ),
               Container(height: 20),
               RaisedButton(
