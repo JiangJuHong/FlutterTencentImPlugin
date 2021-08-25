e07e1007096a1192a7aec23b13baa89d02184a45
蒋具宏 <690717394@qq.com>
2021-08-25 21:45:38 +0800

    feat 优化自定义信令监听器

--- ios/Classes/listener/CustomSignalingListener.swift
+++ ios/Classes/listener/CustomSignalingListener.swift
@@ -8,12 +8,8 @@ import ImSDK_Plus
 /// 自定义信令监听
 class CustomSignalingListener: NSObject, V2TIMSignalingListener {
     ///收到新邀请时
-    func onReceiveNewInvitation(_ inviteID: String!, inviter: String!, groupID: String!, inviteeList: [String]!, data: String!) {
-        var params: [String: Any] = [
-            "inviteID": inviteID!,
-            "data": data!,
-        ];
-
+    func onReceiveNewInvitation(_ inviteID: String!, inviter: String!, groupID: String!, inviteeList: [String]!, data: String?) {
+        var params: [String: Any] = ["inviteID": inviteID!];
         if let temp = inviter {
             params["inviter"] = temp;
         }
@@ -23,37 +19,37 @@ class CustomSignalingListener: NSObject, V2TIMSignalingListener {
         if let temp = inviteeList {
             params["inviteeList"] = temp;
         }
-
-
+        if let temp = data {
+            params["data"] = temp;
+        }
         SwiftTencentImPlugin.invokeListener(type: ListenerType.ReceiveNewInvitation, params: params)
     }
 
-
     /// 被邀请者接受邀请
-    func onInviteeAccepted(_ inviteID: String!, invitee: String!, data: String!) {
-        SwiftTencentImPlugin.invokeListener(type: ListenerType.InviteeAccepted, params: [
-            "inviteID": inviteID!,
-            "invitee": invitee!,
-            "data": data!,
-        ])
+    func onInviteeAccepted(_ inviteID: String!, invitee: String!, data: String?) {
+        var params: [String: Any] = ["inviteID": inviteID!,"invitee": invitee!];
+        if let temp = data {
+            params["data"] = temp;
+        }
+        SwiftTencentImPlugin.invokeListener(type: ListenerType.InviteeAccepted, params: params)
     }
 
     /// 被邀请者拒绝邀请
-    func onInviteeRejected(_ inviteID: String!, invitee: String!, data: String!) {
-        SwiftTencentImPlugin.invokeListener(type: ListenerType.InviteeRejected, params: [
-            "inviteID": inviteID!,
-            "invitee": invitee!,
-            "data": data!,
-        ])
+    func onInviteeRejected(_ inviteID: String!, invitee: String!, data: String?) {
+        var params: [String: Any] = ["inviteID": inviteID!,"invitee": invitee!];
+        if let temp = data {
+            params["data"] = temp;
+        }
+        SwiftTencentImPlugin.invokeListener(type: ListenerType.InviteeRejected, params: params)
     }
 
     /// 邀请被取消
-    func onInvitationCancelled(_ inviteID: String!, inviter: String!, data: String!) {
-        SwiftTencentImPlugin.invokeListener(type: ListenerType.InvitationCancelled, params: [
-            "inviteID": inviteID!,
-            "inviter": inviter!,
-            "data": data!,
-        ])
+    func onInvitationCancelled(_ inviteID: String!, inviter: String!, data: String?) {
+        var params: [String: Any] = ["inviteID": inviteID!,"inviter": inviter!];
+        if let temp = data {
+            params["data"] = temp;
+        }
+        SwiftTencentImPlugin.invokeListener(type: ListenerType.InvitationCancelled, params: params)
     }
 
     /// 邀请超时
